483e8d6d76dd0ca9bcde1d66e4f7cc6913614a0a
Nicholas Ng <nicholasykng@gmail.com>
2019-10-20 03:40:44 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,50 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort {|a, b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  new = [array[0], array[2], array[1]]
+  return new
+end
+
+def reverse_array(element)
+  element.reverse
+end
+
+def kesha_maker(array)
+  array.each do |character|
+    character[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select do |character|
+    character.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  sum = 0
+  array. each do |number|
+    sum += number
+  end
+  sum
+end
+
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
+end
